d5bf1f7936b7b5a9bc2c7a40dcee6ac6e2998837
tanavposwal <tanavposwal939@gmail.com>
2024-12-23 22:03:39 +0530

    tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,100 @@
 import { useQueryState } from "nuqs";
 import MovieInfo from "./component/MovieInfo";
-import { useEffect, useState } from "react";
 import { Search } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+
+async function fetchmovie(keyword: string) {
+  let url = `https://www.omdbapi.com/?t=${keyword}&apikey=967f83fb`;
+  return (await fetch(url)).json();
+}
+
+function Loading() {
+  return (
+    <svg
+      width="40"
+      height="10"
+      viewBox="0 0 120 30"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#fff"
+    >
+      <circle cx="15" cy="15" r="15">
+        <animate
+          attributeName="r"
+          from="15"
+          to="15"
+          begin="0s"
+          dur="0.8s"
+          values="15;9;15"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="fill-opacity"
+          from="1"
+          to="1"
+          begin="0s"
+          dur="0.8s"
+          values="1;.5;1"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="60" cy="15" r="9" fill-opacity="0.3">
+        <animate
+          attributeName="r"
+          from="9"
+          to="9"
+          begin="0s"
+          dur="0.8s"
+          values="9;15;9"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="fill-opacity"
+          from="0.5"
+          to="0.5"
+          begin="0s"
+          dur="0.8s"
+          values=".5;1;.5"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="105" cy="15" r="15">
+        <animate
+          attributeName="r"
+          from="15"
+          to="15"
+          begin="0s"
+          dur="0.8s"
+          values="15;9;15"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="fill-opacity"
+          from="1"
+          to="1"
+          begin="0s"
+          dur="0.8s"
+          values="1;.5;1"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+    </svg>
+  );
+}
 
 function App() {
   const [search, setSearch] = useQueryState("s");
-  const [data, setData] = useState({});
 
-  useEffect(() => {
-    if (search) {
-      fetchmovie({ target: { value: search } });
-    }
-  }, [search]);
-
-  async function fetchmovie(e: any) {
-    let url = `https://www.omdbapi.com/?t=${e.target.value}&apikey=967f83fb`;
-
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      });
-  }
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["movie", search],
+    queryFn: () => fetchmovie(search!),
+    enabled: !!search,
+  });
 
   return (
     <div className="min-h-screen bg-black select-none">
@@ -39,11 +112,21 @@ function App() {
             placeholder="movie name"
           />
         </div>
-        <div className="sm:border border-neutral-700 rounded-md overflow-hidden">
+        <div className="sm:border border-neutral-700 rounded-md overflow-hidden w-full">
           {search ? (
-            <MovieInfo data={data} />
+            isLoading ? (
+              <div className="p-16 text-white flex items-center justify-center">
+                <Loading />
+              </div>
+            ) : error ? (
+              <div className="p-16 text-white flex items-center justify-center">
+                error...
+              </div>
+            ) : (
+              <MovieInfo data={data} />
+            )
           ) : (
-            <div className="p-10 text-white items-center justify-center">
+            <div className="p-16 text-white flex items-center justify-center">
               search for movie...
             </div>
           )}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { NuqsAdapter } from "nuqs/adapters/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <NuqsAdapter>
       <App />
     </NuqsAdapter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
